9526f9ef7a1d39f5b2b5c0a4bfa78d930852d47e
“reddrum” <anton.kranz@me.com>
2017-10-11 22:02:09 +0300

    Add counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+    if session[:counter] > 5
+      return session[:counter]         
+    end
+  end
+
   def index
     @products = Product.order(:title)
+    @count = increment_count
   end
 end
